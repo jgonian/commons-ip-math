4a9c5c7d8fb4a133f22f155f22d57bd17bcb850f
Yannis Gonianakis <ygoniana@ripe.net>
2012-04-12 22:07:19 +0000

    Add parse method for ASNs.
    Implementation copied from commons-internet-resources.

--- src/main/java/net/ripe/commons/ip/resource/Asn.java
+++ src/main/java/net/ripe/commons/ip/resource/Asn.java
@@ -1,5 +1,7 @@
 package net.ripe.commons.ip.resource;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import net.ripe.commons.ip.range.AsnRange;
 import org.apache.commons.lang3.Validate;
 
@@ -15,10 +17,11 @@ public class Asn extends SingleValue<Long> implements SingleInternetResource<Asn
     public static final Asn LAST_16_BIT_ASN = Asn.of(ASN_16_BIT_MAX_VALUE);
     public static final Asn LAST_32_BIT_ASN = Asn.of(ASN_32_BIT_MAX_VALUE);
 
+    private static final Pattern ASN_PATTERN = Pattern.compile("(?:AS)?(\\d+)(\\.(\\d+))?", Pattern.CASE_INSENSITIVE);
+
     public Asn(Long value) {
         super(value);
-        Validate.isTrue(value.compareTo(ASN_MIN_VALUE) >= 0, "Value of ASN has to be greater than or equal to " + ASN_MIN_VALUE);
-        Validate.isTrue(value.compareTo(ASN_32_BIT_MAX_VALUE) <= 0, "Value of ASN has to be less than or equal to " + ASN_32_BIT_MAX_VALUE);
+        validateRange(value, ASN_32_BIT_MAX_VALUE);
     }
 
     public static Asn of(Long value) {
@@ -26,7 +29,36 @@ public class Asn extends SingleValue<Long> implements SingleInternetResource<Asn
     }
 
     public static Asn of(String value) {
-        return new Asn(Long.parseLong(value));
+        return parse(value);
+    }
+
+    public static Asn parse(String text) {
+        if (text == null) {
+            return null;
+        }
+
+        text = text.trim();
+        Matcher matcher = ASN_PATTERN.matcher(text);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(String.format("Invalid ASN: '%s'", text));
+        }
+
+        long low;
+        long high = 0L;
+        if (matcher.group(3) != null) {
+            low = validateRange(Long.valueOf(matcher.group(3)), ASN_16_BIT_MAX_VALUE);
+            high = validateRange(Long.valueOf(matcher.group(1)), ASN_16_BIT_MAX_VALUE);
+        } else {
+            low = Long.valueOf(matcher.group(1));
+        }
+
+        return new Asn((high << 16) | low);
+    }
+
+    private static Long validateRange(Long value, Long max) {
+        Validate.isTrue(value.compareTo(ASN_MIN_VALUE) >= 0, "Value of ASN has to be greater than or equal to " + ASN_MIN_VALUE);
+        Validate.isTrue(value.compareTo(max) <= 0, "Value of ASN has to be less than or equal to " + max);
+        return value;
     }
 
     @Override
