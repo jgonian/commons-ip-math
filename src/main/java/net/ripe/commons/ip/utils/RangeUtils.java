9c1ea5757772059a74a7d3d6ef19d6966a9bb34b
Yannis Gonianakis <ygoniana@ripe.net>
2012-04-19 20:15:35 +0000

    Add RangeUtils class.

---
+++ src/main/java/net/ripe/commons/ip/utils/RangeUtils.java
@@ -0,0 +1,11 @@
+package net.ripe.commons.ip.utils;
+
+public final class RangeUtils {
+
+    public static <T extends Comparable<T>> T rangeCheck(T value, T begin, T end) {
+        if (value.compareTo(begin) >= 0 && value.compareTo(end) <= 0) {
+            return value;
+        }
+        throw new IllegalArgumentException("Value out of range: [" + value + "]");
+    }
+}
