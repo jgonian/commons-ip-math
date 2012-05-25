b4e6eaa64ab75ccd8ae7ce5f6c99ca3e544cdede
Yannis Gonianakis <ygoniana@ripe.net>
2012-05-25 14:47:03 +0000

    Add a method to parse IPv6 addresses.
    This implementation is ported from commons-internet-resources and although it has a good compliance with RFC 4291, parsing IPv6 addresses with embedded IPv4 addresses is flawed and we need to fix it.

--- src/main/java/net/ripe/commons/ip/resource/Ipv6.java
+++ src/main/java/net/ripe/commons/ip/resource/Ipv6.java
@@ -2,7 +2,10 @@ package net.ripe.commons.ip.resource;
 
 import static java.math.BigInteger.*;
 import java.math.BigInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import net.ripe.commons.ip.range.Ipv6Range;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 
 public class Ipv6 extends SingleValue<BigInteger> implements SingleInternetResource<Ipv6, Ipv6Range> {
@@ -16,6 +19,11 @@ public class Ipv6 extends SingleValue<BigInteger> implements SingleInternetResou
 
     public static final Ipv6 FIRST_IPV6_ADDRESS = Ipv6.of(IPv6_MINIMUM_VALUE);
     public static final Ipv6 LAST_IPV6_ADDRESS = Ipv6.of(IPv6_MAXIMUM_VALUE);
+
+    private static final Pattern IPV6_PATTERN_RFC4291 = Pattern.compile("(([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))");
+    private static final Pattern IPV6_PATTERN_EXPANDED = Pattern.compile("([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4})");
+    private static final int COLON_COUNT_FOR_EMBEDDED_IPV4 = 6;
+    private static final int COLON_COUNT_IPV6 = 7;
 
     protected Ipv6(BigInteger value) {
         super(value);
@@ -92,5 +100,62 @@ public class Ipv6 extends SingleValue<BigInteger> implements SingleInternetResou
         }
 
         return sb.toString();
+    }
+
+    /**
+     * Parses a <tt>String</tt> into an {@link Ipv6} address.
+     *
+     * @param ipv6Address a text representation of an IPv6 address as defined in rfc4291
+     * @return a new {@link Ipv6}
+     * @throws NullPointerException if the string argument is <tt>null</tt>
+     * @throws IllegalArgumentException if the string cannot be parsed
+     * @see <a href="http://tools.ietf.org/html/rfc4291">rfc4291 - IP Version 6 Addressing Architecture</a>
+     */
+    public static Ipv6 parse(String ipv6Address) {
+        Validate.notNull(ipv6Address);
+        ipv6Address = ipv6Address.trim();
+        Validate.isTrue(IPV6_PATTERN_RFC4291.matcher(ipv6Address).matches(), "Invalid IPv6 address: " + ipv6Address);
+
+        ipv6Address = expandMissingColons(ipv6Address);
+        if (isInIpv4EmbeddedIpv6Format(ipv6Address)) {
+            ipv6Address = getIpv6AddressWithIpv4SectionInIpv6Notation(ipv6Address);
+        }
+        return new Ipv6(ipv6StringtoBigInteger(ipv6Address));
+    }
+
+    private static String expandMissingColons(String ipAddressString) {
+        int colonCount = isInIpv4EmbeddedIpv6Format(ipAddressString) ? COLON_COUNT_FOR_EMBEDDED_IPV4 : COLON_COUNT_IPV6;
+        return ipAddressString.replace("::", StringUtils.repeat(":", colonCount - StringUtils.countMatches(ipAddressString, ":") + 2));
+    }
+
+    private static boolean isInIpv4EmbeddedIpv6Format(String ipAddressString) {
+        return ipAddressString.contains(".");
+    }
+
+    private static String getIpv6AddressWithIpv4SectionInIpv6Notation(String ipAddressString) {
+        String ipv6Section = StringUtils.substringBeforeLast(ipAddressString, ":");
+        String ipv4Section = StringUtils.substringAfterLast(ipAddressString, ":");
+        try {
+            Ipv4 ipv4 = Ipv4.parse(ipv4Section);
+            Ipv6 ipv6FromIpv4 = new Ipv6(BigInteger.valueOf(ipv4.value()));
+            String ipv4SectionInIpv6Notation = StringUtils.join(ipv6FromIpv4.toString().split(":"), ":", 2, 4);
+            return ipv6Section + ":" + ipv4SectionInIpv6Notation;
+        } catch(IllegalArgumentException e) {
+            throw new IllegalArgumentException("Embedded Ipv4 in IPv6 address is invalid: " + ipAddressString, e);
+        }
+    }
+
+    private static BigInteger ipv6StringtoBigInteger(String ipAddressString) {
+        Matcher m = IPV6_PATTERN_EXPANDED.matcher(ipAddressString);
+        m.find();
+
+        String ipv6Number = "";
+        for (int i = 1; i <= m.groupCount(); i++) {
+            String part = m.group(i);
+            String padding = "0000".substring(0, 4 - part.length());
+            ipv6Number = ipv6Number + padding + part;
+        }
+
+        return new BigInteger(ipv6Number, 16);
     }
 }
--- src/test/java/net/ripe/commons/ip/resource/Ipv6Test.java
+++ src/test/java/net/ripe/commons/ip/resource/Ipv6Test.java
@@ -1,6 +1,8 @@
 package net.ripe.commons.ip.resource;
 
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class Ipv6Test {
@@ -15,5 +17,281 @@ public class Ipv6Test {
     public void shouldToStringLast() {
         Ipv6 addr = Ipv6.LAST_IPV6_ADDRESS;
         assertEquals("ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff", addr.toString());
+    }
+
+    @Test
+    public void shouldParseCompressedAddresses() {
+        // rfc4291 - §2.2) 2.
+        assertEquals(Ipv6.parse("2001:DB8:0:0:8:800:200C:417A"), Ipv6.parse("2001:DB8::8:800:200C:417A"));
+        assertEquals(Ipv6.parse("FF01:0:0:0:0:0:0:101"), Ipv6.parse("FF01::101"));
+        assertEquals(Ipv6.parse("0:0:0:0:0:0:0:1"), Ipv6.parse("::1"));
+        assertEquals(Ipv6.parse("0:0:0:0:0:0:0:0"), Ipv6.parse("::"));
+    }
+
+    @Test
+    public void leadingZerosIn16BitFieldCanBeOmitted() {
+        assertEquals(Ipv6.parse("2001:db8:aaaa:bbbb:cccc:dddd:eeee:0001"), Ipv6.parse("2001:db8:aaaa:bbbb:cccc:dddd:eeee:001"));
+        assertEquals(Ipv6.parse("2001:db8:aaaa:bbbb:cccc:dddd:eeee:0001"), Ipv6.parse("2001:db8:aaaa:bbbb:cccc:dddd:eeee:01"));
+        assertEquals(Ipv6.parse("2001:db8:aaaa:bbbb:cccc:dddd:eeee:0001"), Ipv6.parse("2001:db8:aaaa:bbbb:cccc:dddd:eeee:1"));
+    }
+
+    @Test
+    public void shouldBeAbleToCompressZeros() {
+        assertEquals(Ipv6.parse("2001:db8:aaaa:bbbb:cccc:dddd:0:1"), Ipv6.parse("2001:db8:aaaa:bbbb:cccc:dddd::1"));
+        assertEquals(Ipv6.parse("2001:db8:0:0:0:0:0:1"), Ipv6.parse("2001:db8:0:0:0::1"));
+        assertEquals(Ipv6.parse("2001:db8:0:0:0:0:0:1"), Ipv6.parse("2001:db8:0:0::1"));
+        assertEquals(Ipv6.parse("2001:db8:0:0:0:0:0:1"), Ipv6.parse("2001:db8:0::1"));
+        assertEquals(Ipv6.parse("2001:db8:0:0:0:0:0:1"), Ipv6.parse("2001:db8::1"));
+    }
+
+    @Test
+    public void shouldParseAddressesWithLeadingOrTrailingSpaces() {
+        assertEquals("1:2:3:4:5:6:0:8", Ipv6.parse("   1:2:3:4:5:6::8").toString());
+        assertEquals("1:2:3:4:5:6:0:8", Ipv6.parse("1:2:3:4:5:6::8    ").toString());
+    }
+
+    @Test
+    public void shouldParseDifferentRepresentationsOfTheSameAddress() {
+        Ipv6 ipv6 = Ipv6.parse("2001:db8:0:0:1:0:0:1");
+        ArrayList<Ipv6> list = new ArrayList<Ipv6>();
+        list.add(Ipv6.parse("2001:0db8:0:0:1:0:0:1"));
+        list.add(Ipv6.parse("2001:db8::1:0:0:1"));
+        list.add(Ipv6.parse("2001:db8::0:1:0:0:1"));
+        list.add(Ipv6.parse("2001:0db8::1:0:0:1"));
+        list.add(Ipv6.parse("2001:db8:0:0:1::1"));
+        list.add(Ipv6.parse("2001:db8:0000:0:1::1"));
+        list.add(Ipv6.parse("2001:DB8:0:0:1::1"));
+
+        for (Ipv6 ipv6Variation : list) {
+            assertEquals(ipv6, ipv6Variation);
+        }
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldNotParseNull() {
+        Ipv6.parse(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailOnEmptyString() {
+        Ipv6.parse("");
+    }
+
+    @Test
+    public void shouldParseFullAddressesCaseInsensitively() {
+        assertEquals(Ipv6.parse("abcd:ef01:2345:6789:abcd:ef01:2345:6789"), Ipv6.parse("ABCD:EF01:2345:6789:ABCD:EF01:2345:6789"));
+        assertEquals(Ipv6.parse("2001:db8:0:0:8:800:200c:417a"), Ipv6.parse("2001:DB8:0:0:8:800:200C:417A"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailOnMissingSegment() {
+        Ipv6.parse("1:2:3:4:5:6:7");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailOnExtraSegment() {
+        Ipv6.parse("1:2:3:4:5:6:7:8:9");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotParseIpv6AddressesWithLessThan7ColonsWithoutDoubleColon() {
+        Ipv6.parse("a:b:c");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotParseIpv6AddressesWith7ColonsOnly() {
+        Ipv6.parse(":::::::");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotContainDoubleColonsMoreThanOnce() {
+        Ipv6.parse("2001:db8::aaaa::0:1");
+    }
+
+    @Test
+    public void shouldFailOnInternalSpace() {
+        try {
+            Ipv6.parse("2001:0000:1234: 0000:0000:C1C0:ABCD:0876");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("2001:0000:1234:0000 :0000:C1C0:ABCD:0876");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+    }
+
+    @Test
+    public void shouldFailOnExtraZero() {
+        try {
+            Ipv6.parse("02001:0000:1234:0000:0000:C1C0:ABCD:0876");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("2001:0000:01234:0000:0000:C1C0:ABCD:0876");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+    }
+
+    @Test
+    public void shouldFailOnMultipleDoubleColons() {
+        try {
+            Ipv6.parse("3ffe:b00::1::a");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("::1111:2222:3333:4444:5555:6666::");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+    }
+
+    @Test
+    public void shouldFailIfSegmentOutOfBound() {
+        try {
+            Ipv6.parse("2ffff::10");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("-2::10");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+    }
+
+    // Ipv6 Addresses with Embedded Ipv4 Addresses
+
+    @Test
+    public void shouldParseIpv4CompatibleIpv6Addresses() {
+        // |                80 bits               | 16 |      32 bits        |
+        // +--------------------------------------+--------------------------+
+        // |0000..............................0000|0000|    IPv4 address     |
+        // +--------------------------------------+----+---------------------+
+        assertEquals(Ipv6.parse("0:0:0:0:0:0:1.2.3.4"), Ipv6.parse("::1.2.3.4"));
+        assertEquals(Ipv6.parse("0:0:0:0:0:0:102:304"), Ipv6.parse("::1.2.3.4"));
+    }
+
+    @Test
+    public void shouldParseIpv4MappedIpv6Addresses() {
+        // |                80 bits               | 16 |      32 bits        |
+        // +--------------------------------------+--------------------------+
+        // |0000..............................0000|FFFF|    IPv4 address     |
+        // +--------------------------------------+----+---------------------+
+        assertEquals(Ipv6.parse("0:0:0:0:0:FFFF:1.2.3.4"), Ipv6.parse("::FFFF:1.2.3.4"));
+        assertEquals(Ipv6.parse("0:0:0:0:0:FFFF:102:304"), Ipv6.parse("::FFFF:1.2.3.4"));
+    }
+
+    @Ignore("TODO(ygoniana): Update the parse implementation to comply with §2.5.5. of rfc4291")
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailToParseInvalidIpv4EmbeddedFormat() {
+        Ipv6.parse("a:b:c:d:e:f:13.1.68.3");
+    }
+
+    @Test
+    public void shouldFailIfIpv4PartExceedsBounds() {
+        try {
+            Ipv6.parse("::400.2.3.4");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("::260.2.3.4");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("::256.2.3.4");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("::1.256.3.4");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("::1.2.256.4");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("::1.2.256.256");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("::300.2.3.4");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("::1.300.3.4");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("::1.2.300.4");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("::1.2.3.300");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+    }
+
+    @Test
+    public void shouldFailIfIpv4PartContainsInvalidCharacters() {
+        try {
+            Ipv6.parse("::255Z255X255Y255");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("::192x168.1.26");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+    }
+
+    @Test
+    public void shouldFailIfIpv4PartIsMisplaced() {
+        try {
+            Ipv6.parse("1.2.3.4:FFFF:0000:0000:0000::0000");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+        try {
+            Ipv6.parse("1.2.3.4::");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+    }
+
+    @Test
+    public void shouldFailIfIpv4PartContainsLeadingZeros() {
+        try {
+            Ipv6.parse("::FFFF:254.157.241.086");
+            fail();
+        } catch(IllegalArgumentException e) {
+        }
+    }
+
+    // Ipv6 Ranges
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailToParseRangeInDashNotation() {
+        Ipv6.parse("1:2:3:4-1:2:3:5");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailToParseRangeInCidrNotation() {
+        Ipv6.parse("1:2:3:4/64");
     }
 }
