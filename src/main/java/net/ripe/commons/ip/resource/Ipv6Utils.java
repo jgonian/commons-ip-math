57c50b7020da4c84c95a0bb745082b3152b8b160
Yannis Gonianakis <ygoniana@ripe.net>
2012-05-29 07:25:12 +0000

    Add Ipv6Utils class.
    Contains methods to get the lower and upper bounds of an IPv6 address given a prefix.

---
+++ src/main/java/net/ripe/commons/ip/resource/Ipv6Utils.java
@@ -0,0 +1,22 @@
+package net.ripe.commons.ip.resource;
+
+import static net.ripe.commons.ip.utils.RangeUtils.*;
+import java.math.BigInteger;
+
+public final class Ipv6Utils {
+
+    public static Ipv6 lowerBoundForPrefix(Ipv6 address, int prefixLength) {
+        rangeCheck(prefixLength, 0, Ipv6.IPv6_NUMBER_OF_BITS);
+        BigInteger mask = bitMask(0).xor(bitMask(prefixLength));
+        return new Ipv6(address.value().and(mask));
+    }
+
+    public static Ipv6 upperBoundForPrefix(Ipv6 address, int prefixLength) {
+        rangeCheck(prefixLength, 0, Ipv6.IPv6_NUMBER_OF_BITS);
+        return new Ipv6(address.value().or(bitMask(prefixLength)));
+    }
+
+    protected static BigInteger bitMask(int prefixLength) {
+        return BigInteger.ONE.shiftLeft(Ipv6.IPv6_NUMBER_OF_BITS - prefixLength).add(new BigInteger("-1"));
+    }
+}
