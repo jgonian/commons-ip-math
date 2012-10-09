bc7588be8c6736d4f8fb50900334b5a2f9d656db
Yannis Gonianakis <ygoniana@ripe.net>
2012-10-09 08:22:17 +0000

    Remove obsolete class.

--- commons-ip-math/src/main/java/net/ripe/commons/ip/RangeCreationException.java
+++
@@ -1,8 +0,0 @@
-package net.ripe.commons.ip;
-
-public class RangeCreationException extends RuntimeException {
-
-    public RangeCreationException(String format, Exception e) {
-        super(format, e);
-    }
-}
